26419c9f68f4c12b59592f6b8cdf316bd36c6572
Vinicius <viniciusr66.vr@gmail.com>
2023-05-24 20:05:28 -0300

    metodo para adicionar produto no estoque

--- classes/Estoque.py
+++ classes/Estoque.py
@@ -1,3 +1,15 @@
 class Estoque:
     # lista de produtos em estoque
     produtos: list
+
+    def __init__(self, produtos):
+        self.produtos = produtos
+
+    def coletar(self, produto, quantidade):
+        for prod in self.produtos:
+            if prod["produto"] == produto.id:
+                prod["quantidade"] += quantidade
+                return
+
+        produto_coletado = {"produto": produto.id, "quantidade": quantidade}
+        self.produtos.apppend(produto_coletado)
